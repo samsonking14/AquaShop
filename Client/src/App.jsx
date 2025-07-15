1921cf92c02893bde392e22d3dacd58a921443f5
samsonking14 <samson2049454@gmail.com>
2025-07-15 18:30:55 +0530

    new

---
+++ Client/src/App.jsx
@@ -0,0 +1,23 @@
+import Home from './Components/Pages/Home'
+import About from './Components/Pages/About'
+import Shop from './Components/Pages/Shop'
+import Wishlist from './Components/Pages/Wishlist'
+import Contact from './Components/Pages/Contact'
+import FAQ from './Components/Pages/FAQ'
+import Wrong from './Components/Pages/wrong'
+
+function App() {
+  return (
+    <>
+    <Home />
+    <Shop />
+    <About />
+    <Contact />
+    <FAQ />
+    <Wrong />
+    <Wishlist />        
+    </>
+  )
+}
+
+export default App
